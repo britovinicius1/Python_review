1ba6820514bc507744319a8c754d5c04f9f2c546
Vinicius Brito <brito.vini@Outlook.com>
2026-04-26 18:22:22 -0300

    alguns exemplos do começo

---
+++ exp01.py
@@ -0,0 +1,13 @@
+
+#%%
+
+numero = 10 #int
+numero_decimal = 4.78 #float - decimal
+nome_usuario = "Vinicius" #string
+verdadeiro = True #boolean
+
+##tudo em python tudo é objeto e tudo é estrutura de dados
+#%%
+print(type(nome_usuario))
+
+# %%
