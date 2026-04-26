f85a69322183c37ba0e24891e4ee8b56643c15ab
Vinicius Brito <brito.vini@Outlook.com>
2026-04-26 20:05:37 -0300

    Aula01 Ajustes nos fundamentos

---
+++ aula01/exp01.py
@@ -0,0 +1,15 @@
+##Tudo em python é objeto
+
+# 1 ) Int - Numero inteiro
+# 2 ) Float - Numero com casa decimal
+# 3 )  String -> caracteres de texto
+# 4) Bolean -> True or false
+
+
+numero = 10 
+numero_decimal = 4.78 
+nome_usuario = "Vinicius"
+verdadeiro = True 
+print(type(nome_usuario))
+
+# %%
